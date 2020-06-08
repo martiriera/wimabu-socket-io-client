c72f790bf987411ae306e7c2b8d7fb7a4150f46a
martiriera <martiriera13@gmail.com>
2020-06-08 19:08:30 +0200

    Update for new user auth

--- unidirectional/maps/sendMapInfoActionsUni.js
+++ unidirectional/maps/sendMapInfoActionsUni.js
@@ -4,6 +4,7 @@ var io = require("socket.io-client");
 var mapClientInfo = require("./jsons/mapClientInfoUNI.json");
 var forceAddJSON = require("./jsons/mapForceAdd.json");
 var forceUpdateJSON = require("./jsons/mapForceUpdate.json");
+var userCredentials = require("../../userCredentials.json");
 var { imageToBytea } = require("../../images/imageToBytea");
 
 
@@ -14,9 +15,19 @@ socket.on('error', function (evData) {
 });
 
 socket.on('connected', (data) => {
-    // var mapHasCampaigns = false; //CHANGE IF NECESSARY
     console.log(data);
-    socket.emit('sendMapActions', mapClientInfo);
+
+    // Before sending any actions, submit user credentials
+    socket.emit('sendUserCredentials', userCredentials);
+
+    // When credentials are validated start SYNC sending actions
+    socket.on('userAuth', authRes => {
+        if (authRes) {
+            socket.emit('sendMapActions', mapClientInfo);
+        } else {
+            console.log('Error authenticating user')
+        }
+    })
 
     socket.on('infoActionResult', result => {
         console.log(JSON.stringify(result, null, 2))
