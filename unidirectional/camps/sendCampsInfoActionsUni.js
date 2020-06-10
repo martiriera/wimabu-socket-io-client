16d84bac34768e4daa4f523690c68d0de9e57408
martiriera <martiriera13@gmail.com>
2020-06-10 15:46:01 +0200

    Udpate

--- unidirectional/camps/sendCampsInfoActionsUni.js
+++ unidirectional/camps/sendCampsInfoActionsUni.js
@@ -3,6 +3,7 @@
 var io = require("socket.io-client");
 var campaignClientInfo = require("./jsons/campClientInfoUNI.json");
 var forceAddJSON = require("./jsons/campForceAdd.json");
+var userCredentials = require("../../userCredentials.json");
 
 console.log('Starting connection...');
 var socket = io.connect('http://localhost:8080/sendActions');
@@ -13,15 +14,24 @@ socket.on('error', function (evData) {
 socket.on('connected', (data) => {
     console.log(data);
 
-    socket.emit('sendCampaignActions', campaignClientInfo);
+    // Before sending any actions, submit user credentials
+    socket.emit('sendUserCredentials', userCredentials);
+
+    // When credentials are validated start SYNC sending actions
+    socket.on('userAuth', authRes => {
+        if (authRes) {
+            socket.emit('sendCampaignActions', campaignClientInfo);
+        } else {
+            console.log('Error authenticating user')
+        }
+    });
 
     socket.on('infoActionResult', result => {
         console.log(JSON.stringify(result, null, 2))
         if (result.type === 'MISSING') {
-            // socket.emit('abort')
-            sendForceAdd()
+            socket.emit('abort')
+            // sendForceAdd()
         }
-
     })
 
     function sendForceAdd() {
